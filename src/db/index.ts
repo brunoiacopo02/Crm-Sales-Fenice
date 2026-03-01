c5eaccb1ce273d2917de376764994aada326541a
brunoiacopo02 <brunoiacopo02@users.noreply.github.com>
2026-03-01 23:05:16 +0100

    Fix self signed certificate in pooler

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,6 +7,9 @@ const pool = new Pool({
     max: 10,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export const db = drizzle(pool, { schema });
